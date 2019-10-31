35fd40bd988359d2a7d9f0cb93bc8fc3e9743db7
Abhishek Ranjan <42897204+ranjan103@users.noreply.github.com>
2019-11-01 00:38:01 +0530

    Update data.py

--- sorting/data.py
+++ sorting/data.py
@@ -1,10 +1,4 @@
-# Script Name     : data.py
-# Author          : Howard Zhang
-# Created         : 14th June 2018
-# Last Modified	  : 14th June 2018
-# Version         : 1.0
-# Modifications	  : 
-# Description     : The struct of data to sort and display.
+
 
 class Data:
     # Total of data to sort
